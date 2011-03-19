2678cdbb6db59582cec9d28885e3460f941c5dac
Jonathan Stott <jonathan.stott@gmail.com>
2011-03-19 14:59:13 +0000

    Catch last to_json

--- lib/sovaa.rb
+++ lib/sovaa.rb
@@ -86,7 +86,7 @@ module Sovaa
     def paramify_url url, params = {}
       if params && !params.empty?
         query = params.collect do |k,v|
-          v = v.to_json if %w{key startkey endkey}.include?(k.to_s)
+          v = Yajl::Encoder.encode(v) if %w{key startkey endkey}.include?(k.to_s)
           "#{k}=#{CGI.escape(v.to_s)}"
         end.join("&")
         url = "#{url}?#{query}"
